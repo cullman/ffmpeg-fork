a91132621446dc6a1189dff6298a44538b030755
Elan Feingold <elan@bluemandrill.com>
2008-08-17 20:21:04 -1000

    Don't build shared

--- build_xbmc.sh
+++ build_xbmc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-./configure --extra-cflags="-D_XBOX" --enable-postproc --enable-gpl --enable-static --disable-vhook --enable-swscale --enable-protocol=http --disable-altivec --disable-ipv6 --enable-pthreads --disable-debug 
+./configure --extra-cflags="-D_XBOX" --enable-postproc --enable-gpl --enable-static --disable-vhook --enable-swscale --enable-protocol=http --disable-altivec --disable-ipv6 --enable-pthreads --disable-debug --disable-shared
